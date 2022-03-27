b6e99687e7f06bf1f2d317c55034ed15cb026987
dload1 <89117414+dload1@users.noreply.github.com>
2022-03-27 14:08:46 +0530

    new modified files

--- help.js
+++ help.js
@@ -1,19 +1,26 @@
+// The JS code for displaying/hiding the help/calculator
+
 document.addEventListener("DOMContentLoaded", function () {
   helpButton = document.getElementById("helpButton");
   helpContents = document.getElementById("helpContents");
   closeHelpButton = document.getElementById("closeHelpButton");
   calculator = document.getElementById('calculator');
+
+  // when help button is clicked display the help contents and hide the calculator
+  // note that for help/close buttons i have used the "visibility" so that they are there only but hidden/shown
+  // but for calc/helpContents i have used the "display" property so that they donot affect the layout of eachother when shown/hidden
   helpButton.addEventListener("click", (e) => {
     helpContents.style.display = "block";
     closeHelpButton.style.visibility = "visible";
     helpButton.style.visibility = "hidden";
     calculator.style.display = "none";
   });
+
+  // when the help is closed display the calc and hide the help
   closeHelpButton.addEventListener("click", (e) => {
     helpContents.style.display = "none";
     helpButton.style.visibility = "visible";
     closeHelpButton.style.visibility = "hidden";
     calculator.style.display = "initial";
   });
-
 });
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// The JS code for calculator functions
+
 document.addEventListener("DOMContentLoaded", function () {
-  let ans;
-  // document.getElementById("scientificCalc").style.display = "none";
+
+  // event listeners for showing/hiding the simple/scientific calc
+  // Degree and Radian buttons are shown in scientific calc only
   document.getElementById("simple").addEventListener("click", function () {
     document.getElementById("scientificCalc").style.display = "none";
     document.querySelectorAll("[name='degRad']").forEach((element) => {
@@ -15,9 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  let buttons = document.querySelectorAll(".btn");
-
-  // the following equation holds the field where data is entered
+  // equation holds the field where data is entered
   // result holds the field where calculation result is displayed
   equation = document.getElementById("equationField");
   result = document.getElementById("resultField");
@@ -27,12 +28,55 @@ document.addEventListener("DOMContentLoaded", function () {
   firstFunRows = document.getElementsByClassName("firstFun");
   secondFunRows = document.getElementsByClassName("secondFun");
 
+  // for trignometric functions conversion factor from deg/rad and vice-versa are needed
+  // get all the trignometric functions (sin, cos, tan)
+  let trigno = document.querySelectorAll(".trigno"),
+    // get all the inverse trignometric functions (sin-¹, cos-¹, tan-¹)
+    trignoInv = document.querySelectorAll(".trignoInv");
+
+  // when degree is selected add "(Math.PI/180) * " to trigno and "(180/Math.PI) * " to inverse trignometric functions
+  degree = document.getElementById("degree");
+  degree.addEventListener("click", () => {
+    trigno.forEach((e) => {
+      // check if already the multiplication factor is not there, and add it
+      if (!e.value.includes("(Math.PI/180)")) {
+        e.value += "(Math.PI/180) * ";
+      }
+    });
+
+    trignoInv.forEach((e) => {
+      if (!e.value.includes("(180/Math.PI)")) {
+        e.value = "(180/Math.PI) * " + e.value;
+      }
+    });
+  });
+
+  // remove the multiplication factors added earlier when radian is selected again
+  let radian = document.getElementById("radian");
+  radian.addEventListener("click", () => {
+    trigno.forEach((e) => {
+      if (e.value.includes("(Math.PI/180)")) {
+        // here I have used the ternary operators as the replace() function did not work somehow to remove the factors added earlier
+        e.value.includes("sin") ? (e.value = "Math.sin( ") : false;
+        e.value.includes("cos") ? (e.value = "Math.cos( ") : false;
+        e.value.includes("tan") ? (e.value = "Math.tan( ") : false;
+      }
+    });
+    trignoInv.forEach((e) => {
+      if (e.value.includes("(180/Math.PI)")) {
+        e.value.includes("asin") ? (e.value = "Math.asin( ") : false;
+        e.value.includes("acos") ? (e.value = "Math.acos( ") : false;
+        e.value.includes("atan") ? (e.value = "Math.atan( ") : false;
+      }
+    });
+  });
+
+  // calculate the factorial
   function fact(x) {
     factResult = x;
     if (x < 0 || !Number.isInteger(x))
       return "Factorial is for +ve integers only";
     else if (x == 0) return 1;
-    //else if (!Number.isInteger(x)) return "Factorial is for +ve integers only";
     else {
       for (let i = x - 1; i > 1; i--) {
         factResult *= i;
@@ -41,11 +85,13 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  // calculate the square value
   function square(x) {
     if (isNaN(x)) return NaN;
     else return x * x;
   }
 
+  // calculate the cube value
   function cube(x) {
     if (isNaN(x)) return NaN;
     else return x * x * x;
@@ -62,10 +108,15 @@ document.addEventListener("DOMContentLoaded", function () {
   patternNumber1 = /^[\-]?\d+\.?\d*$/g;
   patternNumber2 = /^[\-]?\d*\.?\d+$/g;
 
+  // get all the button elements
+  let buttons = document.querySelectorAll(".btn");
+  // ans holds the last evaluated result
+  let ans;
+
   // actions to be performed for individual button press
   for (let item of buttons) {
     item.addEventListener("click", function () {
-      // console.log(typeof (equation.value));
+
       switch (item.name) {
         case "clear":
           equation.value = "";
@@ -106,18 +157,23 @@ document.addEventListener("DOMContentLoaded", function () {
           item.innerHTML = "2<sup>nd</sup>";
           break;
 
+        // answer shows the last evaluated result
         case "answer":
           equation.value = ans;
           result.value = "";
           break;
 
+        // change the plus/minus sign of the display/result value
         case "plusMinus":
+          // check if the equation field ends in =, i.e. if it was evaluated
           if (equation.value.slice(-1) == "=") {
-            //console.log(result.value.slice(0, 1))
+            // check if the result field is non-negative, and add "-". Else remove the minus
             if (result.value.slice(0, 1) != "-")
               equation.value = "-" + result.value;
             else equation.value = result.value.slice(1);
-          } else if (equation.value.slice(0, 1) == "-")
+          }
+          // check if the equation field starts with "-", and remove "-". Else add the minus
+          else if (equation.value.slice(0, 1) == "-")
             equation.value = equation.value.slice(1);
           else equation.value = "-" + equation.value;
           break;
@@ -135,19 +191,7 @@ document.addEventListener("DOMContentLoaded", function () {
             } else if (item.classList.contains("plusMinusSymbols")) {
               equation.value = result.value + item.value;
             } else if (item.classList.contains("mathFunctions")) {
-              if (
-                item.classList.contains("trigno") &&
-                document.getElementById("degree").checked == true
-              ) {
-                equation.value =
-                  item.value + "(Math.PI/180) * " + result.value + " ) ";
-              } else if (
-                item.classList.contains("trignoInv") &&
-                document.getElementById("degree").checked == true
-              ) {
-                equation.value =
-                  "(180/Math.PI) * " + item.value + result.value + " ) ";
-              } else equation.value = item.value + result.value + " ) ";
+              equation.value = item.value + result.value + " ) ";
             } else if (item.id == "anyPower") {
               equation.value = item.value + result.value + ", ";
             } else if (item.value == "e") {
@@ -164,23 +208,9 @@ document.addEventListener("DOMContentLoaded", function () {
             patternNumber2.test(equation.value)
           ) {
             if (item.classList.contains("mathFunctions")) {
-              if (
-                item.classList.contains("trigno") &&
-                document.getElementById("degree").checked == true
-              ) {
-                equation.value =
-                  item.value + "(Math.PI/180) * " + equation.value + " )";
-              } else if (
-                item.classList.contains("trignoInv") &&
-                document.getElementById("degree").checked == true
-              ) {
-                equation.value =
-                  "(180/Math.PI) * " + item.value + equation.value + " )";
-              } else equation.value = item.value + equation.value + " )";
+              equation.value = item.value + equation.value + " )";
               result.value = "";
             } else if (item.id == "anyPower") {
-              //console.log("equation is: "+ equation.value);
-              //console.log("item is: " + item.value);
               equation.value = item.value + equation.value + ", ";
             } else if (item.name == "e") {
               equation.value += item.value;
@@ -188,16 +218,6 @@ document.addEventListener("DOMContentLoaded", function () {
           }
 
           // for any other key presses:
-          // if any +-/*% symbol is pressed enter a space before and after
-          /*  else if (item.classList.contains("plusMinusSymbols")) {
-                  //console.log("equation is: "+ equation.value);
-                  //console.log("item is: " + item.value);
-                  equation.value += " ";
-                  equation.value += item.value;
-                  equation.value += " ";
-                }*/
-
-          // for others directly enter the key without space
           else equation.value += item.value;
           break;
       }
